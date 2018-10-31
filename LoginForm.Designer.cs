3864442758da8254540200daeadf8ca9a2b348ec
QQ652276536 <652276536@qq.com>
2018-10-31 18:21:31 +0800

    update

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -81,7 +81,7 @@ namespace Login
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(1, 367);
+            this.panel1.Location = new System.Drawing.Point(0, 367);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(338, 66);
             this.panel1.TabIndex = 1;
